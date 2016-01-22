09dc96dc7b17feb509da2848ea68bbf541c74943
* * * A S H * * * <madl-ash@users.noreply.github.com>
2016-01-22 12:13:23 +0330

    Making flask multi Threaded
    for faster responds to client

--- app.py
+++ app.py
@@ -263,4 +263,4 @@ def signout():
 #run the app.
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='127.0.0.1',port = 5001)
+    app.run(host='127.0.0.1', port = 5001, threaded=True)
